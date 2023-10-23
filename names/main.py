6c6a0007db577c2c47114ce93bb6121a5d0dee6d
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-23 15:50:08 +0200

    lunes1

--- names/main.py
+++ names/main.py
@@ -1,11 +1,33 @@
 import pandas as pd
+from os import listdir
+import re
 
-df = pd.read_csv("names/yob2015.txt", header=None, names=["nombre", "sexo", "total"])
-df.set_index(["nombre", "sexo"], inplace=True)
-print(df.head(10))
 
-df2 = pd.read_csv("names/yob2016.txt", header=None, names=["nombre", "sexo", "total"])
-df2.set_index(["nombre", "sexo"], inplace=True)
-suma = df.add(df2, fill_value=0)
-suma.sort_values(by="total", ascending=False, inplace=True)
-print(suma.head(10))
+def cargaDataFrame(path):
+    df = pd.read_csv(path, header=None, names=["nombre", "sexo", "total"])
+    df.set_index(["nombre", "sexo"], inplace=True)
+    return df
+
+
+def sumarDosDF():
+    df = cargaDataFrame("names/yob2015.txt")
+    df2 = cargaDataFrame("names/yob2016.txt")
+    suma = df.add(df2, fill_value=0)
+    suma.sort_values(by="total", ascending=False, inplace=True)
+    print(suma.head(10))
+
+
+def seleccionarFicheros(path, añoini, añofin):
+    # Seleccionar el rango de ficheros a partir de los parámetros:
+    patron = "yob(\d{4})\.txt"
+    for f in listdir(path):
+        obj = re.match(patron, f)
+        if obj:
+            año = int(obj.groups()[0])
+            if añoini <= año <= añofin:
+                print(f)
+
+
+if __name__ == "__main__":
+    # sumarDosDF()
+    seleccionarFicheros("../practicas/Analisis datos/names/", 1990, 1995)
