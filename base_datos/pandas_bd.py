ca2977326000864a47424e119d94ac50f420dfe9
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-25 16:21:36 +0200

    mie1

--- base_datos/pandas_bd.py
+++ base_datos/pandas_bd.py
@@ -1,8 +1,30 @@
 import sqlite3 as dbapi
 from os.path import isfile
+import pandas as pd
 
-path = "empresa3.db"
-if isfile(path):
-    print("ok")
-else:
-    raise FileNotFoundError(f"No existe el fichero {path}")
+
+def listarPedidos():
+    path = "empresa3.db"
+    if isfile(path):
+        con = dbapi.connect(path)
+        cur = con.cursor()
+        sql = "select * from pedidos"
+        cur.execute(sql)
+        for t in cur.fetchall():
+            print(t)
+        cur.close()
+        con.close()
+    else:
+        raise FileNotFoundError(f"No existe el fichero {path}")
+
+
+def cargarPedidos():
+    path = "empresa3.db"
+    if isfile(path):
+        con = dbapi.connect(path)
+        sql = "select * from pedidos"
+        df = pd.read_sql(sql, con)
+        print(df.head())
+        con.close()
+
+cargarPedidos()
