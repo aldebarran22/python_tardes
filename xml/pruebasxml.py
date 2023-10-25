5bc37145303aec648a72589e5051bdcdbffbb390
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-10-25 17:23:58 +0200

    mier2234

--- xml/pruebasxml.py
+++ xml/pruebasxml.py
@@ -16,6 +16,20 @@ def cargaDOM(path):
             paises.add(nodo.text)
     print("paises:", sorted(paises))
 
+    # Sumar los pedidos de Brasil:
+    total = 0
+    for nodo in tree.iter():
+        if nodo.tag == "importe":
+            importe = float(nodo.text.strip())
+
+        if nodo.tag == "pais":
+            pais = nodo.text.strip()
+            if pais == "Brasil":
+                print("pais:", pais, " importe: ", importe)
+                total += importe
+
+    print(f"El total de Brasil es: {total}")
+
 
 if __name__ == "__main__":
     cargaDOM("Pedidos.xml")
